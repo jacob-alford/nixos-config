320392679a8f5fb2b49d0c17bfb85f2a91e9157d
Jacob Alford <github.scouting378@passmail.net>
2024-09-19 20:57:36 -0600

    add vscode and vscode config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -262,6 +262,92 @@
     };
   };
 
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      catppuccin.catppuccin-vsc
+      catppuccin.catppuccin-vsc-icons
+      streetsidesoftware.code-spell-checker
+      dbaeumer.vscode-eslint
+      esbenp.prettier-vscode
+      vscodevim.vim
+      rust-lang.rust-analyzer
+      haskell.haskell
+      davidanson.vscode-markdownlint
+      elmtooling.elm-ls-vscode
+      unifiedjs.vscode-mdx
+    ];
+
+    userSettings = {
+      "editor.fontFamily" = "Victor Mono";
+      "editor.snippetSuggestions" = "none";
+      "terminal.integrated.fontFamily" = "Victor Mono";
+      "files.insertFinalNewline" = true;
+      "editor.fontLigatures" = true;
+      "editor.formatOnSave" = true;
+      "editor.suggest.showWords" = false;
+      "editor.acceptSuggestionOnCommitCharacter" = false;
+      "emmet.showExpandedAbbreviation" = "never";
+      "editor.renderLineHighlight" = "none";
+
+      # Minimal mist conifg
+
+      "editor.scrollbar.vertical" = "hidden";
+      "window.commandCenter" = false;
+      "window.titleBarStyle" = "custom";
+      "breadcrumbs.enabled" = true;
+      "workbench.sideBar.location" = "right";
+      "workbench.statusBar.visible" = true;
+      "workbench.tree.indent" = 22;
+      "workbench.tree.renderIndentGuides" = "onHover";
+
+      # ###
+
+      "editor.tabSize" = 2;
+      "editor.bracketPairColorization.enabled" = true;
+      "editor.detectIndentation" = false;
+      "javascript.suggest.autoImports" = false;
+      "typescript.suggest.autoImports" = false;
+      "typescript.preferences.importModuleSpecifier" = "relative";
+      "editor.lineNumbers" = "relative";
+
+      "editor.tokenColorCustomizations" = {
+        "textMateRules" = [
+          {
+            "scope" = [
+              "comment"
+              "storage.modifier"
+              "storage.type.php"
+              "keyboard.other.new.php"
+              "entity.other.attribute-name"
+              "fenced_code.block.language.markdown"
+              "keyboard"
+              "storage.modifier"
+              "storage.type"
+              "keyboard.control"
+              "constant.language"
+              "entity.other.attribute-name"
+              "entity.name.method"
+              "keyboard.control.import.ts"
+              "keyboard.control.import.tsx"
+              "keyboard.control.import.js"
+              "keyboard.control.flow.js"
+              "keyboard.control.from.js"
+              "keyboard.control.from.ts"
+              "keyboard.control.from.tsx"
+            ];
+            "settings" = {
+              "fontStyle" = "italic";
+            };
+          }
+        ];
+      };
+
+      "workbench.colorTheme" = "Catppuccin Frappé";
+      "workbench.iconTheme" = "catppuccin-frappe";
+    };
+  };
+
   home.packages = with pkgs; [
     steam
     protonup
