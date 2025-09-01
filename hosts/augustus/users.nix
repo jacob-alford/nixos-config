527bc2e7e4f1ae573836ae983d98e7c8d30d0e90
= <github.scouting378@passmail.net>
2025-08-31 21:11:27 -0600

    add iPad ssh key

--- hosts/augustus/users.nix
+++ hosts/augustus/users.nix
@@ -14,6 +14,7 @@
       packages = with pkgs; [ ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMtyqHcqkr11WdGh8DQYCY8rUrq0NwfwWMcYio3Z9Wof"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEaAzzPDHwyEe/3DS6MlYlAjZpdg84KckJsBA99k0/O3NgS16/XMj2SH/JN6mrcV/GZ6yat60RlpuYbgKBNDl84="
       ];
       shell = pkgs.zsh;
     };
