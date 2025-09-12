5ce9a0f8fea913b7b23afa6b1f8a3ab8bb7a0d28
Jacob Alford <jacob@mini.local>
2025-09-12 14:49:31 -0600

    add keys to ts secrets

--- shared/nix-darwin.nix
+++ shared/nix-darwin.nix
@@ -12,9 +12,9 @@
       settings = {
         experimental-features = "nix-command flakes";
         # Opinionated: disable global registry
-        # flake-registry = "";
+        flake-registry = "";
         # Workaround for https://github.com/NixOS/nix/issues/9574
-        # nix-path = config.nix.nixPath;
+        nix-path = config.nix.nixPath;
 
         allowed-users = [ "@wheel" ];
       };
