accc49e9129f991b818dc215b5f36d5cf860486c
= <github.scouting378@passmail.net>
2025-11-02 12:52:17 -0700

    add yk1 ssh key to aug

--- hosts/augustus/users.nix
+++ hosts/augustus/users.nix
@@ -25,6 +25,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMtyqHcqkr11WdGh8DQYCY8rUrq0NwfwWMcYio3Z9Wof"
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEaAzzPDHwyEe/3DS6MlYlAjZpdg84KckJsBA99k0/O3NgS16/XMj2SH/JN6mrcV/GZ6yat60RlpuYbgKBNDl84="
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMSTyHOqx+TEq++NCjw3gvdQ8pDgndfLmWuNvMGMjAHgAAAABHNzaDo= yk1-ssh-ed25519"
       ];
       shell = pkgs.zsh;
     };
