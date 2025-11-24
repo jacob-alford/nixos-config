12f2b702d633f0e5e40f61320ccc907ce94c9279
Jacob Alford <github.scouting378@passmail.net>
2025-11-24 10:51:35 -0700

    update nixos ssh keys

--- hosts/nixos/users.nix
+++ hosts/nixos/users.nix
@@ -12,6 +12,7 @@
       description = "Jacob Alford";
       openssh.authorizedKeys.keys = [
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBD6gE/UA8NCZkxImg073c02kzh3P6ohV8DLzTXQeoJanwCDJgWYMsQI55XoYqanK8n/xooiKEkt3MCIAmG9EtTs="
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGCI/SI5sFnBLVyn4kit2WXh/tgFhoblW8CNF68HLD4yMU3lN52QD0BCSe1s3R1NTnZGnGxaQANR7EEKjQBvEjc="
       ];
       extraGroups = [ "networkmanager" "wheel" ];
       packages = with pkgs; [ ];
