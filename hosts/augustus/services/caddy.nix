3f9c30304d172c5d5440e6c4c39f4b99ca013de1
Jacob Alford <github.scouting378@passmail.net>
2026-01-06 20:36:56 -0700

    update caddy hash

--- hosts/augustus/services/caddy.nix
+++ hosts/augustus/services/caddy.nix
@@ -13,7 +13,7 @@
         # Doesn't currently support 2.10.2
         # "github.com/mholt/caddy-l4@v0.0.0-20250829174953-ad3e83c51edb"
       ];
-      hash = "sha256-vH3Y5gFAKsCll2Wq3jxQCMWT4G0OwHSGn94y5/eP7zw=";
+      hash = "sha256-XQpSWgI/7E9FvWw2EMEOyl/rkddgLWd3SSusCmViMwo=";
     };
 
     enable = true;
