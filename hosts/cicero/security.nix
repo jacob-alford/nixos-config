add21020d6fa84dbd7b1e289b3d5dee032510e04
Jacob Alford <github.scouting378@passmail.net>
2026-01-11 16:11:14 -0700

    add pcsc to cicero

--- hosts/cicero/security.nix
+++ hosts/cicero/security.nix
@@ -10,6 +10,24 @@
 
   security.pki.certificateFiles = [ ../../certs/alford-root.crt ];
 
+  # Enable PC/SC daemon for YubiKey support
+  services.pcscd.enable = true;
+
+  # Allow all processes to access PC/SC for YubiKey
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.debian.pcsc-lite.access_card") {
+        return polkit.Result.YES;
+      }
+    });
+
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.debian.pcsc-lite.access_pcsc") {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
   networking.firewall = {
     enable = true;
 
