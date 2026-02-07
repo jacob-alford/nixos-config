59e5d397f71f267c33a849a3f0e5fe0fb5b63161
Jacob Alford <jacob@mini.local>
2026-02-07 09:41:36 -0700

    update caddy log location, remove step-ca

--- hosts/mini/services.nix
+++ hosts/mini/services.nix
@@ -10,17 +10,16 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [
-    pkgs.step-ca
     pkgs.step-cli
   ];
 
   launchd.daemons.caddy = {
-    command = "${pkgs.caddy}/bin/caddy run --config ${./Caddyfile}";
+    command = "${pkgs.caddy}/bin/caddy run --config ${./Caddyfile} --adapter caddyfile";
     serviceConfig = {
       KeepAlive = true;
       RunAtLoad = true;
-      StandardOutPath = "/Users/jacob/Documents/caddy_jacob.out.log";
-      StandardErrorPath = "/Users/jacob/Documents/caddy_jacob.err.log";
+      StandardOutPath = "/etc/caddy/logs/caddy.info.log";
+      StandardErrorPath = "/etc/caddy/logs/caddy.err.log";
     };
   };
 }
