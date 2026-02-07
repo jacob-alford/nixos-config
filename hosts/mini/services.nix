3c51c62fefed0a1f0799e747fc6ecd9154f6c2bc
Jacob Alford <github.scouting378@passmail.net>
2026-02-07 09:27:00 -0700

    update mini config for cicero

--- hosts/mini/services.nix
+++ hosts/mini/services.nix
@@ -1,46 +1,26 @@
-{ inputs
-, outputs
-, lib
-, config
-, pkgs
-, ...
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
 }:
-let
-  caddyMod = pkgs.caddy.withPlugins {
-    plugins = [
-      "github.com/tailscale/caddy-tailscale@v0.0.0-20250508175905-642f61fea3cc"
-      "github.com/mholt/caddy-l4@v0.0.0-20250530154005-4d3c80e89c5f"
-    ];
-    hash = "sha256-dS/vz9jgs6l9ZeFGaFvHPC4HwciOGNeGbO7ad+qmQMc=";
-  };
-in
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [
-    caddyMod
     pkgs.step-ca
     pkgs.step-cli
   ];
 
-  launchd.user.agents.caddy = {
-    command = "${caddyMod}/bin/caddy run --config /etc/nix-darwin/Caddyfile --envfile=\"${config.sops.templates."tailscale.env".path}\"";
+  launchd.daemons.caddy = {
+    command = "${pkgs.caddy}/bin/caddy run --config ${./Caddyfile}";
     serviceConfig = {
       KeepAlive = true;
       RunAtLoad = true;
       StandardOutPath = "/Users/jacob/Documents/caddy_jacob.out.log";
       StandardErrorPath = "/Users/jacob/Documents/caddy_jacob.err.log";
-    };
-  };
-
-  launchd.daemons.step = {
-    command = "${pkgs.step-ca}/bin/step-ca /etc/step/config/ca.json";
-    serviceConfig = {
-      KeepAlive = true;
-      RunAtLoad = true;
-      StandardOutPath = "/etc/step/logs/step.out.log";
-      StandardErrorPath = "/etc/step/logs/step.err.log";
-      UserName = "_step";
     };
   };
 }
--- hosts/mini/sops.nix
+++ hosts/mini/sops.nix
@@ -1,53 +1,16 @@
-{ inputs
-, lib
-, config
-, pkgs
-, ...
-}: {
-  sops.defaultSopsFile = ../../secrets/tailscale.yaml;
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  sops.defaultSopsFile = ../../secrets/mini.yaml;
   sops.defaultSopsFormat = "yaml";
 
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   sops.age.keyFile = "Users/jacob/Library/Application\ Support/sops/age/keys.txt";
   sops.age.generateKey = true;
-
-  sops.secrets.jellyfin = {
-    owner = "jacob";
-  };
-  sops.secrets.ai = {
-    owner = "jacob";
-  };
-  sops.secrets."home-assistant" = {
-    owner = "jacob";
-  };
-  sops.secrets.ollama = {
-    owner = "jacob";
-  };
-  sops.secrets.affine = {
-    owner = "jacob";
-  };
-  sops.secrets.umbrel = {
-    owner = "jacob";
-  };
-  sops.secrets."dev-tools" = {
-    owner = "jacob";
-  };
-  sops.secrets."ca" = {
-    owner = "jacob";
-  };
-
-  sops.templates."tailscale.env" = {
-    content = ''
-      JELLYFIN_TS_KEY=${config.sops.placeholder."jellyfin"}
-      OPENWEBUI_TS_KEY=${config.sops.placeholder."ai"}
-      HOMEASSISTANT_TS_KEY=${config.sops.placeholder."home-assistant"}
-      OLLAMA_TS_KEY=${config.sops.placeholder."ollama"}
-      AFFINE_TS_KEY=${config.sops.placeholder."affine"}
-      UMBREL_TS_KEY=${config.sops.placeholder."umbrel"}
-      DEV_TOOLS_TS_KEY=${config.sops.placeholder."dev-tools"}
-      CA_TS_KEY=${config.sops.placeholder."ca"}
-    '';
-    owner = "jacob";
-  };
 }
